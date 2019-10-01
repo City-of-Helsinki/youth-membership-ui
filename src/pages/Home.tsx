c0818897b3650b5e02bbfed19483a505fd910954
Jimi <jiripim@gmail.com>
2019-10-01 15:35:02 +0300

    Create a basic layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {RouteProps} from 'react-router';
 
-import Header from '../common/header/Header';
+import PageLayout from '../common/layout/PageLayout';
 
-type Props = RouteProps & {
-
-};
+type Props = RouteProps & {};
 
 
 function Home(props: Props) {
   return (
-    <Header/>
+    <PageLayout>
+      Home
+    </PageLayout>
   );
 }
 
