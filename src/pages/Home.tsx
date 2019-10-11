1759ea36e021c8e8ff0347a05a9420172f91b8fa
Jimi <jiripim@gmail.com>
2019-10-11 13:44:12 +0300

    Add button to initiate tunnistamo flow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { RouteProps } from 'react-router';
+import { RouteChildrenProps } from 'react-router';
 
+import userManager from '../oidc/userManager';
 import PageLayout from '../common/layout/PageLayout';
 
-type Props = RouteProps & {};
+type Props = RouteChildrenProps & {};
 
 function Home(props: Props) {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <button onClick={() => userManager.signinRedirect()}>
+        Authenticate using tunnistamo
+      </button>
+    </PageLayout>
+  );
 }
 
 export default Home;
