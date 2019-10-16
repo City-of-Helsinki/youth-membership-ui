03e2b8084b47722ca9f7a14343dcb01607e895d7
Jimi <jiripim@gmail.com>
2019-10-16 15:42:02 +0300

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router';
 
 import App from './App';
+import configureStore from './redux/store';
+
+const store = configureStore({});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <MemoryRouter>
-      <App />
+      <App store={store} />
     </MemoryRouter>,
     div
   );
