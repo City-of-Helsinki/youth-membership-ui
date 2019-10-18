eacb3256dd152f8646d56cea9afc06ca5cb38bfd
Jimi <jiripim@gmail.com>
2019-10-18 16:55:17 +0300

    Expose redux state type

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux-starter-kit';
 import { reducer as oidcReducer } from 'redux-oidc';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   oidc: oidcReducer,
 });
+
+export default rootReducer;
+export type AppState = ReturnType<typeof rootReducer>;
