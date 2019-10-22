037586a1aad715747653350a21663bad57a3c505
Jimi <jiripim@gmail.com>
2019-10-22 11:51:13 +0300

    Refactor userManager.getUser()-call to own helper

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,16 +1,18 @@
 import ApolloClient from 'apollo-boost';
 
-import userManager from '../oidc/userManager';
+import getAuthenticatedUser from '../oidc/getAuthenticatedUser';
 
 export default new ApolloClient({
   request: async operation => {
-    const user = await userManager.getUser();
-    if (user) {
+    try {
+      const user = await getAuthenticatedUser();
       operation.setContext({
         headers: {
           Authorization: `Bearer ${user.access_token}`,
         },
       });
+    } catch (e) {
+      // User not authenticated
     }
   },
   uri: process.env.REACT_APP_PROFILE_GRAPHQL,
