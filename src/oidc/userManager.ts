67bc4c34524abe729dcfc2ac9f716d128dbcdcb2
Jimi <jiripim@gmail.com>
2019-10-23 13:37:28 +0300

    Disable @typescript-eslint/camelcase

--- src/oidc/userManager.ts
+++ src/oidc/userManager.ts
@@ -5,7 +5,6 @@ const location = `${window.location.protocol}//${window.location.hostname}${
   window.location.port ? `:${window.location.port}` : ''
 }`;
 
-/* eslint-disable @typescript-eslint/camelcase */
 const settings: UserManagerSettings = {
   authority: process.env.REACT_APP_OIDC_AUTHORITY,
   client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
@@ -13,7 +12,6 @@ const settings: UserManagerSettings = {
   response_type: 'id_token token',
   scope: process.env.REACT_APP_OIDC_SCOPE,
 };
-/* eslint-enable @typescript-eslint/camelcase */
 
 const userManager = createUserManager(settings);
 
