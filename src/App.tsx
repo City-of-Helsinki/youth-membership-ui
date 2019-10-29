bab475d4953824d31ab2e2a4711fa2cd55cdbf95
Jimi <jiripim@gmail.com>
2019-10-29 17:31:59 +0200

    Load the user and api-token when app mounts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router';
 import { Provider as ReduxProvider } from 'react-redux';
 import { OidcProvider, loadUser } from 'redux-oidc';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import store from './redux/store';
-import userManager from './oidc/userManager';
-import enableOidcLogging from './oidc/enableOidcLogging';
+import userManager from './auth/userManager';
+import enableOidcLogging from './auth/enableOidcLogging';
 import graphqlClient from './graphql/client';
 import Home from './pages/Home';
 import OidcCallback from './pages/OidcCallback';
+import { fetchApiTokenThunk } from './auth/redux';
 
 if (process.env.NODE_ENV !== 'production') {
   enableOidcLogging();
 }
 
-loadUser(store, userManager);
-
 type Props = {};
 
 function App(props: Props) {
+  useEffect(() => {
+    loadUser(store, userManager).then(async user => {
+      if (user) {
+        store.dispatch(fetchApiTokenThunk(user.access_token));
+      }
+    });
+  }, []);
   return (
     <ReduxProvider store={store}>
       <OidcProvider store={store} userManager={userManager}>
