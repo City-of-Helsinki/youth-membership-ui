669ba2680b1a89798b40397318ff2838a38b63d8
Jimi <jiripim@gmail.com>
2019-10-30 16:36:47 +0200

    Implement silent renew

--- src/auth/userManager.ts
+++ src/auth/userManager.ts
@@ -12,10 +12,12 @@ const location = `${window.location.protocol}//${window.location.hostname}${
 /* eslint-disable @typescript-eslint/camelcase */
 const settings: UserManagerSettings = {
   authority: process.env.REACT_APP_OIDC_AUTHORITY,
+  automaticSilentRenew: true,
   client_id: process.env.REACT_APP_OIDC_CLIENT_ID,
   redirect_uri: `${location}/callback`,
   response_type: 'id_token token',
   scope: process.env.REACT_APP_OIDC_SCOPE,
+  silent_redirect_uri: `${location}/silent_renew`,
 };
 /* eslint-enable @typescript-eslint/camelcase */
 
