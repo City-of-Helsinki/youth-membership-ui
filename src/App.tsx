18bf6ad9e768a3f8787db21c6e4e26d2a44dcaca
Jimi Pirilä <jimi.pirila@digia.com>
2019-11-01 16:34:19 +0200

    Fetch API-tokens
    Fetch api-tokens from tunnistamo
    Implement silent renew

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import { OidcProvider, loadUser } from 'redux-oidc';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import store from './redux/store';
-import userManager from './oidc/userManager';
-import enableOidcLogging from './oidc/enableOidcLogging';
+import userManager from './auth/userManager';
+import enableOidcLogging from './auth/enableOidcLogging';
 import graphqlClient from './graphql/client';
 import Home from './pages/Home';
 import OidcCallback from './pages/OidcCallback';
+import { fetchApiTokenThunk } from './auth/redux';
 
 if (process.env.NODE_ENV !== 'production') {
   enableOidcLogging();
 }
 
-loadUser(store, userManager);
+loadUser(store, userManager).then(async user => {
+  if (user) {
+    store.dispatch(fetchApiTokenThunk(user.access_token));
+  }
+});
 
 type Props = {};
 
@@ -25,6 +30,13 @@ function App(props: Props) {
       <OidcProvider store={store} userManager={userManager}>
         <ApolloProvider client={graphqlClient}>
           <Switch>
+            <Route
+              path="/silent_renew"
+              render={() => {
+                userManager.signinSilentCallback();
+                return null;
+              }}
+            />
             <Route path="/callback" component={OidcCallback} />
             <Route path="/" component={Home} exact />
           </Switch>
