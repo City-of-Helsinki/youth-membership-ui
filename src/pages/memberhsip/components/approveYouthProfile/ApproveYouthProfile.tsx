5e877cac822e869b6093768e486d45f1d970ad49
Santtu <santtu.tuovinen2@gmail.com>
2020-01-09 14:47:28 +0200

    Changed query & added page after approval

--- src/pages/memberhsip/components/approveYouthProfile/ApproveYouthProfile.tsx
+++ src/pages/memberhsip/components/approveYouthProfile/ApproveYouthProfile.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useParams } from 'react-router';
 import { loader } from 'graphql.macro';
+import { useTranslation } from 'react-i18next';
 
+import Loading from '../../../../common/loading/Loading';
 import convertBooleanToString from '../../helpers/convertBooleantoString';
 import convertDateToFi from '../../helpers/convertDateToFi';
 import PageLayout from '../../../../common/layout/PageLayout';
+import ConfirmApprovingYouthProfile from '../confirmApprovingYouthProfile/ConfirmApprovingYouthProfile';
 import ApproveYouthProfileForm, {
   FormValues,
 } from '../approveYouthProfileForm/ApproveYouthProfileForm';
-import { MyProfileQuery } from '../../graphql/__generated__/MyProfileQuery';
+import { YouthProfileByApprovalToken } from '../../graphql/__generated__/YouthProfileByApprovalToken';
 import {
   ApproveYouthProfile as ApproveYourProfileData,
   ApproveYouthProfileVariables,
 } from '../../graphql/__generated__/ApproveYouthProfile';
 
-const MY_PROFILE = loader('../../graphql/MyProfileQuery.graphql');
+const PROFILE_BY_TOKEN = loader(
+  '../../graphql/YouthProfileByApprovalToken.graphql'
+);
 const APPROVE_PROFILE = loader('../../graphql/ApproveYouthProfile.graphql');
 
 type Props = {};
@@ -24,10 +29,15 @@ type Params = {
 };
 
 function ApproveYouthProfile(props: Props) {
-  const { data } = useQuery<MyProfileQuery>(MY_PROFILE);
-
+  const [approvalSuccessful, setApprovalSuccessful] = useState(false);
   const params = useParams<Params>();
-
+  const { t } = useTranslation();
+  const { data, loading: queryLoading } = useQuery<YouthProfileByApprovalToken>(
+    PROFILE_BY_TOKEN,
+    {
+      variables: { token: params.token },
+    }
+  );
   const [approveProfile, { loading }] = useMutation<
     ApproveYourProfileData,
     ApproveYouthProfileVariables
@@ -40,51 +50,76 @@ function ApproveYouthProfile(props: Props) {
         approverLastName: values.approverLastName,
         approverEmail: values.approverEmail,
         approverPhone: values.approverPhone,
-        birthDate: data?.myProfile?.youthProfile?.birthDate,
+        birthDate: data?.youthProfileByApprovalToken?.birthDate,
       },
       approvalToken: params.token,
     };
 
     approveProfile({ variables }).then(result => {
-      // Todo add history.push here to redirect user after successful approve
       if (result.data) {
+        setApprovalSuccessful(true);
       }
     });
   };
 
   return (
     <PageLayout background="adult">
-      <ApproveYouthProfileForm
-        profile={{
-          firstName: data?.myProfile?.firstName || '',
-          lastName: data?.myProfile?.lastName || '',
-          address:
-            data?.myProfile?.primaryAddress?.address +
-              ', ' +
-              data?.myProfile?.primaryAddress?.postalCode +
-              ', ' +
-              data?.myProfile?.primaryAddress?.city || '',
-          email: data?.myProfile?.primaryEmail?.email || '',
-          phone: data?.myProfile?.primaryPhone?.phone || '',
-          birthDate:
-            convertDateToFi(data?.myProfile?.youthProfile?.birthDate) || '',
-          schoolName: data?.myProfile?.youthProfile?.schoolName || '',
-          schoolClass: data?.myProfile?.youthProfile?.schoolClass || '',
-          approverFirstName:
-            data?.myProfile?.youthProfile?.approverFirstName || '',
-          approverLastName:
-            data?.myProfile?.youthProfile?.approverLastName || '',
-          approverPhone: data?.myProfile?.youthProfile?.approverPhone || '',
-          approverEmail: data?.myProfile?.youthProfile?.approverEmail || '',
-          photoUsageApproved:
-            convertBooleanToString(
-              data?.myProfile?.youthProfile?.photoUsageApproved
-            ) || 'false',
-          languageAtHome: data?.myProfile?.youthProfile?.languageAtHome || '',
-        }}
-        isSubmitting={loading}
-        onValues={handleOnValues}
-      />
+      <Loading
+        isLoading={queryLoading}
+        loadingClassName="asd"
+        loadingText={t('loading')}
+      >
+        {!approvalSuccessful && data && (
+          <ApproveYouthProfileForm
+            profile={{
+              firstName:
+                data?.youthProfileByApprovalToken?.profile?.firstName || '',
+              lastName:
+                data?.youthProfileByApprovalToken?.profile?.lastName || '',
+              address:
+                data?.youthProfileByApprovalToken?.profile?.primaryAddress
+                  ?.address +
+                  ', ' +
+                  data?.youthProfileByApprovalToken?.profile?.primaryAddress
+                    ?.postalCode +
+                  ', ' +
+                  data?.youthProfileByApprovalToken?.profile?.primaryAddress
+                    ?.city || '',
+              email:
+                data?.youthProfileByApprovalToken?.profile?.primaryEmail
+                  ?.email || '',
+              phone:
+                data?.youthProfileByApprovalToken?.profile?.primaryPhone
+                  ?.phone || '',
+              birthDate:
+                convertDateToFi(data?.youthProfileByApprovalToken?.birthDate) ||
+                '',
+              schoolName: data?.youthProfileByApprovalToken?.schoolName || '',
+              schoolClass: data?.youthProfileByApprovalToken?.schoolClass || '',
+              approverFirstName:
+                data?.youthProfileByApprovalToken?.approverFirstName || '',
+              approverLastName:
+                data?.youthProfileByApprovalToken?.approverLastName || '',
+              approverPhone:
+                data?.youthProfileByApprovalToken?.approverPhone || '',
+              approverEmail:
+                data?.youthProfileByApprovalToken?.approverEmail || '',
+              photoUsageApproved:
+                convertBooleanToString(
+                  data?.youthProfileByApprovalToken?.photoUsageApproved
+                ) || 'false',
+              languageAtHome:
+                data?.youthProfileByApprovalToken?.languageAtHome || '',
+            }}
+            isSubmitting={loading}
+            onValues={handleOnValues}
+          />
+        )}
+        {!approvalSuccessful && !data && (
+          <h2>Hakemus on jo hyväksytty / linkki on viallinen</h2>
+        )}
+        {approvalSuccessful && <ConfirmApprovingYouthProfile />}
+      </Loading>
     </PageLayout>
   );
 }
