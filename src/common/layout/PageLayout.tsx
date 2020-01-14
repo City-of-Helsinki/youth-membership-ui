126db8d45b08eb487599df8f44b0d4cc1f439789
Santtu <santtu.tuovinen2@gmail.com>
2020-01-14 14:54:31 +0200

    css changes for footer.

--- src/common/layout/PageLayout.tsx
+++ src/common/layout/PageLayout.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 
 import Header from '../header/Header';
 import HostingBox from '../hostingBox/HostingBox';
+import Footer from '../footer/Footer';
 import styles from './PageLayout.module.css';
 
 type Props = {
@@ -12,14 +14,16 @@ type Props = {
 function PageLayout(props: Props) {
   return (
     <div
-      className={
+      className={classNames(
+        styles.background,
         props.background === 'youth'
           ? styles.youthBackground
           : styles.adultBackground
-      }
+      )}
     >
       <Header />
       <HostingBox className={styles.hostingBox}>{props.children}</HostingBox>
+      <Footer />
     </div>
   );
 }
