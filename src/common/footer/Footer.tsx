8c35b0c20c3dc492793d277b2fd736d1f2d8d267
Santtu <santtu.tuovinen2@gmail.com>
2020-01-15 07:55:55 +0200

    Links plus translations for footer.

--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Koros } from 'hds-react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import tosConstants from '../../pages/memberhsip/constants/tosConstants';
 import { ReactComponent as HelsinkiLogo } from '../svg/HelsinkiLogo.svg';
 import styles from './Footer.module.css';
 
 function Footer() {
+  const { t, i18n } = useTranslation();
+  const link = Object(tosConstants.REGISTER_DESCRIPTION)[
+    i18n.language.toUpperCase()
+  ];
   return (
     <React.Fragment>
       <Koros className={styles.koros} />
@@ -14,21 +21,22 @@ function Footer() {
           <hr />
           <div className={styles.textContainer}>
             <span>
-              &copy; Copyright 2020 &bull; Kaikki oikeudet pidätetään &bull;{' '}
-              <Link to="/#" className={styles.links}>
-                Rekisteriseloste
-              </Link>{' '}
+              &copy; {t('footer.copyright')} &bull; {t('footer.reserveRights')}{' '}
+              &bull;{' '}
+              <a href={link} className={styles.links}>
+                {t('footer.privacy')}
+              </a>{' '}
               | 
               <Link to="/#" className={styles.links}>
-                Saavutettavuusseloste
+                {t('footer.accessibility')}
               </Link>{' '}
               | 
-              <Link to="/#" className={styles.links}>
-                Palvelunkäyttöehdot
+              <Link to="/terms-of-service" className={styles.links}>
+                {t('footer.terms')}
               </Link>
             </span>
             <Link to="/#" className={styles.feedback}>
-              Anna palautetta
+              {t('footer.feedback')}
             </Link>
           </div>
         </div>
