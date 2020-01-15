b586031461ca98352b3c144aaf70c1252138440e
Santtu <santtu.tuovinen2@gmail.com>
2020-01-15 13:04:20 +0200

    Changed copyright year to variable.

--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -10,8 +10,9 @@ import styles from './Footer.module.css';
 function Footer() {
   const { t, i18n } = useTranslation();
   const selectedLanguage =
-    (i18n.language && i18n.language.toUpperCase()) || 'FI';
+    (i18n.languages && i18n.languages[0].toUpperCase()) || 'FI';
   const link = Object(tosConstants.REGISTER_DESCRIPTION)[selectedLanguage];
+  const year = new Date().getFullYear();
   return (
     <React.Fragment>
       <Koros className={styles.koros} />
@@ -21,8 +22,8 @@ function Footer() {
           <hr />
           <div className={styles.textContainer}>
             <span>
-              &copy; {t('footer.copyright')} &bull; {t('footer.reserveRights')}{' '}
-              &bull;{' '}
+              &copy; {t('footer.copyright', { year })} &bull;{' '}
+              {t('footer.reserveRights')} &bull;{' '}
               <a href={link} className={styles.links}>
                 {t('footer.privacy')}
               </a>{' '}
