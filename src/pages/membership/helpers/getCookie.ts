870434c5a7c903254518cc22cce9077079424e0c
Santtu <santtu.tuovinen2@gmail.com>
2020-02-11 12:50:39 +0200

    Cleaned up with prettier.

--- src/pages/membership/helpers/getCookie.ts
+++ src/pages/membership/helpers/getCookie.ts
@@ -1,9 +1,9 @@
 export default function getCookie(cookieName: string) {
-  const pattern = RegExp(cookieName + "=.[^;]*");
-  let matched = document.cookie.match(pattern);
-  if(matched){
-    let cookie = matched[0].split('=');
-    return cookie[1]
+  const pattern = RegExp(cookieName + '=.[^;]*');
+  const matched = document.cookie.match(pattern);
+  if (matched) {
+    const cookie = matched[0].split('=');
+    return cookie[1];
   }
-  return ''
+  return '';
 }
