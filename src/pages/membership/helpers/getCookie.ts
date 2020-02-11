ac8cc71f6ee95c44133d38ed51b90660fb7bd266
Santtu <santtu.tuovinen2@gmail.com>
2020-02-11 12:44:35 +0200

    Changed to regex instead of loop

--- src/pages/membership/helpers/getCookie.ts
+++ src/pages/membership/helpers/getCookie.ts
@@ -1,16 +1,9 @@
 export default function getCookie(cookieName: string) {
-  const name = cookieName + '=';
-  const decodeCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodeCookie.split(';');
-
-  for (let i = 0; i < cookieArray.length; i = i + 1) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(name) === 0) {
-      return cookie.substring(name.length, cookie.length);
-    }
+  const pattern = RegExp(cookieName + "=.[^;]*");
+  let matched = document.cookie.match(pattern);
+  if(matched){
+    let cookie = matched[0].split('=');
+    return cookie[1]
   }
-  return '';
+  return ''
 }
