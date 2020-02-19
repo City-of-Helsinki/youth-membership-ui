6105f1322537b02f6c5c3a86d4f13220384aa03c
Santtu <santtu.tuovinen2@gmail.com>
2020-02-19 08:25:22 +0200

    Added graphql query for name.

--- src/graphql/generatedTypes.ts
+++ src/graphql/generatedTypes.ts
@@ -278,6 +278,34 @@ export interface MembershipDetails {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL query operation: NameQuery
+// ====================================================
+
+export interface NameQuery_myProfile {
+  readonly __typename: "ProfileNode";
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly nickname: string;
+}
+
+export interface NameQuery {
+  /**
+   * Get the profile belonging to the currently authenticated user.
+   * 
+   * Requires authentication.
+   * 
+   * Possible error codes:
+   * 
+   * * `TODO`
+   */
+  readonly myProfile: NameQuery_myProfile | null;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: RenewMyYouthProfile
 // ====================================================
 
