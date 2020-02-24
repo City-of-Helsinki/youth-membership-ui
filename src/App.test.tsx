070e71db0e04556ebe8751dffcf4baab2384c56a
Santtu <santtu.tuovinen2@gmail.com>
2020-02-24 13:27:53 +0200

    Added testing libraries and some test utils.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
+import { withRouter } from 'react-router';
 
 import App from './App';
 
+const AppWithRouter = withRouter(App);
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<AppWithRouter />);
 });
