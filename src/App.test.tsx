63d504080c53ac092d44848410eda98ba7323f5a
Santtu <santtu.tuovinen2@gmail.com>
2020-02-25 07:20:41 +0200

    Remove withRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { withRouter } from 'react-router';
 
 import App from './App';
 
-const AppWithRouter = withRouter(App);
-
 it('renders without crashing', () => {
-  shallow(<AppWithRouter />);
+  shallow(<App />);
 });
