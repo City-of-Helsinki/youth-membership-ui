deed235e3c0f625af30d91959829a4dc51ef2295
Santtu Tuovinen <santtu.tuovinen2@gmail.com>
2020-04-28 14:31:20 +0300

    Added helper function for user age checking.

--- src/pages/membership/components/youthProfileForm/YouthProfileForm.tsx
+++ src/pages/membership/components/youthProfileForm/YouthProfileForm.tsx
@@ -12,6 +12,13 @@ import { Language, YouthLanguage } from '../../../../graphql/generatedTypes';
 import styles from './YouthProfileForm.module.css';
 import Button from '../../../../common/button/Button';
 
+const isConsentRequired = (birthDate: string, schema: Yup.StringSchema) => {
+  const userAge = differenceInYears(new Date(), new Date(birthDate));
+  return userAge < ageConstants.ADULT
+    ? schema.required('validation.required')
+    : schema;
+};
+
 const schema = Yup.object().shape({
   firstName: Yup.string()
     .min(2, 'validation.tooShort')
@@ -41,44 +48,25 @@ const schema = Yup.object().shape({
   approverFirstName: Yup.string()
     .min(2, 'validation.tooShort')
     .max(255, 'validation.tooLong')
-    .when(['birthDate'], (value: string, schema: Yup.StringSchema) => return isConsentRequired(birthDate, schema)
-    
-    const isConsentRequired = (birthDate:string, schema: Yup.StringSchema) => {
-      const userAge = differenceInYears(new Date(), new Date(value));
-      return userAge < ageConstants.ADULT
-        ? schema.required('validation.required')
-        : schema;
-    }
-      const userAge = differenceInYears(new Date(), new Date(value));
-      return userAge < ageConstants.ADULT
-        ? schema.required('validation.required')
-        : schema;
-    }),
+    .when(['birthDate'], (birthDate: string, schema: Yup.StringSchema) =>
+      isConsentRequired(birthDate, schema)
+    ),
   approverLastName: Yup.string()
     .min(2, 'validation.tooShort')
     .max(255, 'validation.tooLong')
-    .when(['birthDate'], (value: string, schema: Yup.StringSchema) => {
-      const userAge = differenceInYears(new Date(), new Date(value));
-      return userAge < ageConstants.ADULT
-        ? schema.required('validation.required')
-        : schema;
-    }),
+    .when(['birthDate'], (birthDate: string, schema: Yup.StringSchema) =>
+      isConsentRequired(birthDate, schema)
+    ),
   approverPhone: Yup.string()
     .min(6, 'validation.phoneMin')
-    .when(['birthDate'], (value: string, schema: Yup.StringSchema) => {
-      const userAge = differenceInYears(new Date(), new Date(value));
-      return userAge < ageConstants.ADULT
-        ? schema.required('validation.required')
-        : schema;
-    }),
+    .when(['birthDate'], (birthDate: string, schema: Yup.StringSchema) =>
+      isConsentRequired(birthDate, schema)
+    ),
   approverEmail: Yup.string()
     .email('validation.email')
-    .when(['birthDate'], (value: string, schema: Yup.StringSchema) => {
-      const userAge = differenceInYears(new Date(), new Date(value));
-      return userAge < ageConstants.ADULT
-        ? schema.required('validation.required')
-        : schema;
-    }),
+    .when(['birthDate'], (birthDate: string, schema: Yup.StringSchema) =>
+      isConsentRequired(birthDate, schema)
+    ),
   photoUsageApproved: Yup.string().required('validation.required'),
   terms: Yup.boolean().oneOf([true], 'validation.required'),
 });
