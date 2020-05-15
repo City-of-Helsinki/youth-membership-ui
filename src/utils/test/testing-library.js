bcd0e8a8997fccfb899d76be7869c63c1534817f
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-05-15 15:28:08 +0300

    Add testing-library

---
+++ src/utils/test/testing-library.js
@@ -0,0 +1,15 @@
+// https://testing-library.com/docs/react-testing-library/setup
+import { render } from '@testing-library/react';
+
+const AllTheProviders = ({ children }) => {
+  return children;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
