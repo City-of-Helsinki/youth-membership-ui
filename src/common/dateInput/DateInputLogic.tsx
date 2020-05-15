34fc6b665c49a9acef3128a976caf0a0aadee88c
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-05-15 14:44:08 +0300

    Refactor date input
    - Fix non-numeric characters being allowed on iOS
    - Fix negative numbers being allowed on any platform
    - Set maximum length for numbers in character counts. Two for date and
      month inputs, four for year input.
    - Automatically change focus into next input when previous input has
      been filled

---
+++ src/common/dateInput/DateInputLogic.tsx
@@ -0,0 +1,240 @@
+import React from 'react';
+import defaultTo from 'lodash/defaultTo';
+
+const DATE_INPUT_ALLOWED_CHARACTERS = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+];
+const INPUT_CONFIGS = {
+  // We are invoking type number in order to allow platforms invoke
+  // behavior that allows more convenient  number input.
+  type: 'number',
+  // Sets the minimum allowed number as 0 for chrome controls. The user
+  // is still able to input a negative number by hand if they choose.
+  min: 0,
+  // 1) Ensures that a number pad is shown on iOS instead of the regular
+  // keyboard
+  // 2) Blocks iOS devices from inputting characters other than numbers
+  pattern: '\\d*',
+};
+
+type DateObject = {
+  date?: number;
+  month?: number;
+  year?: number;
+};
+
+export interface InputComponentProps {
+  id: string;
+  name: string;
+  onChange: (event: React.FormEvent<HTMLInputElement>) => void;
+  value?: string;
+  innerRef?: React.RefObject<HTMLInputElement>;
+  min: number;
+  pattern: string;
+  type: string;
+  isInvalid: boolean;
+  label: string;
+}
+
+export interface WrapperComponentProps {
+  children?: React.ReactNode;
+  isInvalid: boolean;
+}
+
+interface Props {
+  onChange: (date: Date) => void;
+  wrapper: React.FC<WrapperComponentProps>;
+  value: Date | null;
+  isInvalid: boolean;
+  dateInputId: string;
+  monthInputId: string;
+  yearInputId: string;
+  dateInputName: string;
+  monthInputName: string;
+  yearInputName: string;
+  dateComponent: React.FC<InputComponentProps>;
+  monthComponent: React.FC<InputComponentProps>;
+  yearComponent: React.FC<InputComponentProps>;
+  dateInputLabel: string;
+  monthInputLabel: string;
+  yearInputLabel: string;
+}
+
+function getDateComponents(date: Date | null): DateObject {
+  if (!date) {
+    return {};
+  }
+
+  const monthDate = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  return {
+    date: monthDate,
+    month,
+    year,
+  };
+}
+
+function makeDate(date: DateObject): Date | null {
+  if (date.year && date.month && date.date) {
+    return new Date(date.year, date.month, date.date, 0, 0, 0, 0);
+  }
+
+  return null;
+}
+
+function getCharacterCount(number: number): number {
+  return number.toString().length;
+}
+
+function sanitizeInput(content: unknown) {
+  return String(content)
+    .split('')
+    .filter(character => DATE_INPUT_ALLOWED_CHARACTERS.includes(character))
+    .join('');
+}
+
+function cutToLength(string: string, length: number) {
+  if (string.length <= length) {
+    return string;
+  }
+
+  return string.slice(0, length);
+}
+
+function parseNumberFromValue(value: string, maxCharacterCount = 2): number {
+  // Removes all other characters than numbers from the result. This is
+  // a last fallback which makes negative numbers positive and removes
+  // possible characters from the output. The behavior of
+  // input[type="number"] is not consistent between platforms.
+  const sanitized = sanitizeInput(value);
+  const maxSized = cutToLength(sanitized, maxCharacterCount);
+
+  return Number(maxSized);
+}
+
+function DateInputLogic({
+  onChange,
+  value,
+  isInvalid,
+  wrapper,
+  dateInputId,
+  monthInputId,
+  yearInputId,
+  dateInputName,
+  monthInputName,
+  yearInputName,
+  dateComponent,
+  monthComponent,
+  yearComponent,
+  dateInputLabel,
+  monthInputLabel,
+  yearInputLabel,
+}: Props) {
+  const currentDate = getDateComponents(value);
+  const [cachedDate, setCachedDate] = React.useState<DateObject>(currentDate);
+  const monthInputRef = React.useRef(null);
+  const yearInputRef = React.useRef(null);
+
+  const updateDate = (
+    date: number | null,
+    month: number | null,
+    year: number | null
+  ) => {
+    setCachedDate(previousDate => {
+      const nextCachedDate = {
+        date: defaultTo(date, previousDate.date),
+        month: defaultTo(month, previousDate.month),
+        year: defaultTo(year, previousDate.year),
+      };
+      const nextDate = makeDate(nextCachedDate);
+
+      if (nextDate) {
+        onChange(nextDate);
+      }
+
+      return nextCachedDate;
+    });
+  };
+
+  const focusWhen = (
+    condition: boolean,
+    element: React.RefObject<HTMLInputElement>
+  ) => {
+    if (condition && element.current) {
+      element.current.focus();
+    }
+  };
+
+  const handleDateChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const nextDate = parseNumberFromValue(event.currentTarget.value);
+
+    updateDate(nextDate, null, null);
+    focusWhen(getCharacterCount(nextDate) === 2, monthInputRef);
+  };
+  const handleMonthChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const nextMonth = parseNumberFromValue(event.currentTarget.value);
+    // User's input month number, we save month index into state
+    const nextMonthIndex = nextMonth - 1;
+
+    updateDate(null, nextMonthIndex, null);
+    focusWhen(getCharacterCount(nextMonth) === 2, yearInputRef);
+  };
+  const handleYearChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const nextYear = parseNumberFromValue(event.currentTarget.value, 4);
+
+    updateDate(null, null, nextYear);
+  };
+
+  const formattedMonth =
+    cachedDate.month !== undefined
+      ? (cachedDate.month + 1).toString()
+      : undefined;
+
+  return React.createElement(wrapper, { isInvalid }, [
+    React.createElement(dateComponent, {
+      key: dateInputId,
+      id: dateInputId,
+      name: dateInputName,
+      value: defaultTo(cachedDate.date?.toString(), ''),
+      onChange: handleDateChange,
+      isInvalid,
+      label: dateInputLabel,
+      ...INPUT_CONFIGS,
+    }),
+    React.createElement(monthComponent, {
+      key: monthInputId,
+      id: monthInputId,
+      name: monthInputName,
+      value: defaultTo(formattedMonth, ''),
+      onChange: handleMonthChange,
+      innerRef: monthInputRef,
+      isInvalid,
+      label: monthInputLabel,
+      ...INPUT_CONFIGS,
+    }),
+    React.createElement(yearComponent, {
+      key: yearInputId,
+      id: yearInputId,
+      name: yearInputName,
+      value: defaultTo(cachedDate.year?.toString(), ''),
+      onChange: handleYearChange,
+      innerRef: yearInputRef,
+      isInvalid,
+      label: yearInputLabel,
+      ...INPUT_CONFIGS,
+    }),
+  ]);
+}
+
+export default DateInputLogic;
---
+++ src/common/dateInput/__tests__/DateInput.test.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+
+import {
+  render,
+  // eslint-disable-next-line import/named
+  fireEvent,
+  // eslint-disable-next-line import/named
+  getByLabelText,
+} from '../../../utils/test/testing-library';
+import DateInput from '../DateInput';
+
+describe('<DateInput />', () => {
+  const dateInputLabel = 'date-input';
+  const monthInputLabel = 'month-input';
+  const yearInputLabel = 'year-input';
+  const defaultProps = {
+    value: '',
+    onChange: jest.fn(),
+    dateInputLabel,
+    monthInputLabel,
+    yearInputLabel,
+  };
+  const getWrapper = props =>
+    render(<DateInput {...defaultProps} {...props} />);
+  const dateInputSelector = container =>
+    getByLabelText(container, dateInputLabel);
+  const monthInputSelector = container =>
+    getByLabelText(container, monthInputLabel);
+  const yearInputSelector = container =>
+    getByLabelText(container, yearInputLabel);
+
+  it('should render expectedly', () => {
+    expect(toJson(getWrapper())).toMatchSnapshot();
+  });
+
+  it('should not allow text to be inputted', () => {
+    const { container, queryByDisplayValue } = getWrapper();
+
+    const testNoTextAllowed = inputSelector => {
+      const testString = 'text';
+
+      fireEvent.change(inputSelector(container), {
+        target: { value: testString },
+      });
+
+      expect(queryByDisplayValue(testString)).toEqual(null);
+    };
+
+    testNoTextAllowed(dateInputSelector);
+    testNoTextAllowed(monthInputSelector);
+    testNoTextAllowed(yearInputSelector);
+  });
+
+  it('should not allow negative numbers', () => {
+    const { container, queryByDisplayValue } = getWrapper();
+
+    const testNoNegativeAllowed = (inputSelector, testValue) => {
+      fireEvent.change(inputSelector(container), {
+        target: { value: `-${testValue}` },
+      });
+
+      expect(queryByDisplayValue(`-${testValue}`)).toEqual(null);
+      expect(queryByDisplayValue(testValue.toString())).toBeDefined();
+    };
+
+    testNoNegativeAllowed(dateInputSelector, 3);
+    testNoNegativeAllowed(monthInputSelector, 4);
+    testNoNegativeAllowed(yearInputSelector, 2015);
+  });
+
+  it('should have a maximum character count of 2 for date and month fields, and 4 for year field', () => {
+    const { container, queryByDisplayValue } = getWrapper();
+
+    const testNoTooLongNumberAllowed = (inputSelector, numberLength) => {
+      const forSureTooLongTestValue = Array.from({ length: 1000 })
+        .map(() => 'a')
+        .join('');
+
+      fireEvent.change(inputSelector(container), {
+        target: { value: forSureTooLongTestValue },
+      });
+
+      const expectedValue = forSureTooLongTestValue.slice(0, numberLength);
+
+      expect(queryByDisplayValue(forSureTooLongTestValue)).toEqual(null);
+      expect(queryByDisplayValue(expectedValue)).toBeDefined();
+    };
+
+    testNoTooLongNumberAllowed(dateInputSelector, 2);
+    testNoTooLongNumberAllowed(monthInputSelector, 2);
+    testNoTooLongNumberAllowed(yearInputSelector, 4);
+  });
+
+  it.skip('should transition focus after two characters have been inputted', () => {
+    // This has to be tested with cypress, jsdom doesn't have focus
+    // support
+  });
+});
