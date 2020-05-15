4dfb17ba839f242fd84b23b705b64fd881bdbb12
Santtu Tuovinen <santtu.tuovinen2@gmail.com>
2020-05-15 08:35:40 +0300

    Filter based on length.

--- src/i18n/languageSwitcher/LanguageSwitcher.tsx
+++ src/i18n/languageSwitcher/LanguageSwitcher.tsx
@@ -5,9 +5,9 @@ import classNames from 'classnames';
 import styles from './LanguageSwitcher.module.css';
 
 const languages = [
-  { code: 'fi', twoPartCode: 'fi-FI', label: 'Suomi' },
-  { code: 'sv', twoPartCode: 'sv-SV', label: 'Svenska' },
-  { code: 'en', twoPartCode: 'en-US', label: 'English' },
+  { code: 'fi', label: 'Suomi' },
+  { code: 'sv', label: 'Svenska' },
+  { code: 'en', label: 'English' },
 ];
 
 type Props = {
@@ -24,25 +24,26 @@ function LanguageSwitcher(props: Props) {
     }
   };
 
+  const filteredLanguages = languages.filter(lang => {
+    if (i18n.languages[0].length > 2) {
+      return i18n.languages[0].substr(0, 2) !== lang.code;
+    }
+    return i18n.languages[0] !== lang.code;
+  });
+
   return (
     <ul className={classNames(props.className, styles.list)}>
-      {languages
-        .filter(
-          lang =>
-            lang.code !== i18n.languages[0] &&
-            lang.twoPartCode !== i18n.languages[0]
-        )
-        .map(lang => (
-          <li key={lang.code} className={styles.item}>
-            <button
-              type="button"
-              lang={lang.code}
-              onClick={() => setLanguage(lang.code)}
-            >
-              {lang.label}
-            </button>
-          </li>
-        ))}
+      {filteredLanguages.map(lang => (
+        <li key={lang.code} className={styles.item}>
+          <button
+            type="button"
+            lang={lang.code}
+            onClick={() => setLanguage(lang.code)}
+          >
+            {lang.label}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
