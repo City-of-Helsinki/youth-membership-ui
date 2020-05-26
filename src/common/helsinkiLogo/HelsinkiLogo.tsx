ab05c100e1372fc309090474e4d3e908fb22a63d
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-05-26 13:38:28 +0300

    Logo is now a link.

--- src/common/helsinkiLogo/HelsinkiLogo.tsx
+++ src/common/helsinkiLogo/HelsinkiLogo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
 import classNames from 'classnames';
 
 import styles from './HelsinkiLogo.module.css';
@@ -9,16 +10,23 @@ type Props = {
 };
 function HelsinkiLogo({ className }: Props) {
   const { t, i18n } = useTranslation();
+  const history = useHistory();
+
+  const navigateToHomePage = () => history.push('/');
 
   if (i18n.languages[0] === 'sv')
     return (
       <span
+        role="button"
+        onClick={navigateToHomePage}
         className={classNames(styles.logoSv, className)}
         aria-label={t('helsinkiLogo')}
       />
     );
   return (
     <span
+      role="button"
+      onClick={navigateToHomePage}
       className={classNames(styles.logoFi, className)}
       aria-label={t('helsinkiLogo')}
     />
