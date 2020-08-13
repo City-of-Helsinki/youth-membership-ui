0c0514d91b8ad94f35d738ac58c4d1c0e29211ec
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-08-13 09:07:56 +0300

    Add clarifying comment

--- src/domain/youthProfile/helpers/prepareArrayFieldChanges.ts
+++ src/domain/youthProfile/helpers/prepareArrayFieldChanges.ts
@@ -6,6 +6,22 @@ type ArrayFieldChanges<Add, Update, Remove> = {
   remove: Remove;
 };
 
+// In the form we just have an array of models. But when we send them to
+// the API, we have to distinct between new ones, ones that are updates
+// and ones that are being removed.
+
+// Form data model
+// model: [
+//     {...}
+// ]
+
+// API data model
+// addModel: [...],
+// updateModel: [...],
+// removeModel: [...],
+
+// So this function uses the previous values and the next values to
+// understand which model is new, which is updated and which is removed.
 function prepareArrayFieldChanges<C, M extends Model>(
   current: M[],
   next: Array<M | Omit<C, 'id'>>
