6a16c089ac2b0d997fad244748f042b2d2de8250
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-09-03 16:12:07 +0300

    Use less code heavy way of imitating onBlur
    I didn't know before that React allows to attach onBlur handlers with
    so little setup to divs. This approach uses way less code and likely
    doesn't introduce problematic corner cases as it relies on core library
    features.

--- src/common/components/dateInput/DateInputRow.tsx
+++ src/common/components/dateInput/DateInputRow.tsx
@@ -1,18 +1,30 @@
 import React, { useRef } from 'react';
 import classNames from 'classnames';
 
-import useFocusWithinOut from './useFocusWithinOut';
 import { WrapperComponentProps } from './DateInputLogic';
 import styles from './dateInputRow.module.css';
 
 function DateInputRow({ isInvalid, onBlur, ...rest }: WrapperComponentProps) {
   const wrapperElement = useRef(null);
-  useFocusWithinOut(wrapperElement, onBlur);
 
   return (
     <div
       {...rest}
       ref={wrapperElement}
+      // In react div elements can have onBlur without any extra
+      // configuration.
+      onBlur={e => {
+        // This onBlur handler captures all the blur events that happen
+        // within it. For instance, the events created when moving
+        // between inputs, bubble to this handler. For that purpose we
+        // have to apply logic to ensure that we only call onBlur when
+        // the focus leaves this wrapping component.
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        if (e && !e.currentTarget.contains(e.relatedTarget) && onBlur) {
+          onBlur();
+        }
+      }}
       className={classNames(styles.row, { [styles.invalid]: isInvalid })}
     />
   );
