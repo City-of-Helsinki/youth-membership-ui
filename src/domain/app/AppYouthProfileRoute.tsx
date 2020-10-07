1140b833a085335804284e74447e6bfec5748447
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-10-07 10:55:31 +0300

    Revert to useQuery.

--- src/domain/app/AppYouthProfileRoute.tsx
+++ src/domain/app/AppYouthProfileRoute.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router';
-import { useLazyQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { loader } from 'graphql.macro';
 import * as Sentry from '@sentry/browser';
 import { useTranslation } from 'react-i18next';
 import { isValid, parseISO } from 'date-fns';
-import { useSelector } from 'react-redux';
 
 import { HasYouthProfile } from '../../graphql/generatedTypes';
 import getCookie from '../../common/helpers/getCookie';
 import LoadingContent from '../../common/components/loading/LoadingContent';
 import NotificationComponent from '../../common/components/notification/NotificationComponent';
-import { isAuthenticatedSelector } from '../auth/redux';
+import authConstants from '../auth/constants/authConstants';
 
 const HAS_YOUTH_PROFILE = loader(
   '../youthProfile/graphql/HasYouthProfile.graphql'
@@ -23,21 +22,19 @@ function AppYouthProfileRoute(props: Props) {
   const { t } = useTranslation();
   const [showNotification, setShowNotification] = useState<boolean>(false);
 
-  const [loadProfile, { data, loading }] = useLazyQuery<HasYouthProfile>(
-    HAS_YOUTH_PROFILE,
-    {
-      onError: (error: Error) => {
-        Sentry.captureException(error);
-        setShowNotification(true);
-      },
-    }
-  );
-
-  const isAuthenticated = useSelector(isAuthenticatedSelector);
-
-  useEffect(() => {
-    if (isAuthenticated) loadProfile();
-  }, [isAuthenticated, loadProfile]);
+  const { data, loading } = useQuery<HasYouthProfile>(HAS_YOUTH_PROFILE, {
+    onError: error => {
+      // Ignore permission errors. This decision was made based on the difficulty to add enough checks that slow down Redirecting.
+      // Using useLazyQuery with isAuthenticatedSelector won't fetch profile information fast enough and user is always
+      // redirected to LoginPage. If in the future permission error causes application to crash, some kind of custom hook is necessary.
+      error.graphQLErrors.forEach(qlError => {
+        if (qlError.message !== authConstants.PERMISSION_DENIED) {
+          Sentry.captureException(qlError);
+          setShowNotification(true);
+        }
+      });
+    },
+  });
 
   const isYouthProfileFound = Boolean(data?.myProfile?.youthProfile);
   const birthDate = getCookie('birthDate');
--- src/domain/auth/constants/authConstants.ts
+++ src/domain/auth/constants/authConstants.ts
@@ -3,4 +3,5 @@ export default {
     YOUTH_CENTERS: 'https://palvelukartta.hel.fi/fi/search?q=nuorisotalo',
   },
   OIDC_KEY: `oidc.user:${process.env.REACT_APP_OIDC_AUTHORITY}:${process.env.REACT_APP_OIDC_CLIENT_ID}`,
+  PERMISSION_DENIED: 'You do not have permission to perform this action',
 };
