05f07b3b0b8877051152b79a2a9ce1feb5c9fb48
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-10-27 06:32:13 +0200

    Improve approval view.

--- src/graphql/errorCodes.ts
+++ src/graphql/errorCodes.ts
@@ -1,5 +1,6 @@
 export const ErrorCodes = Object.freeze({
   PERMISSION_DENIED_ERROR: 'PERMISSION_DENIED_ERROR',
+  OBJECT_DOES_NOT_EXIST_ERROR: 'OBJECT_DOES_NOT_EXIST_ERROR',
 } as const);
 
 export type ErrorCode = typeof ErrorCodes[keyof typeof ErrorCodes];
