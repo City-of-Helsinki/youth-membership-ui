ebbe2b71452533827942c0978da6dd28f9c24e51
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-01-15 09:42:18 +0200

    Snapshot test should now work.

--- src/common/components/wrapper/__tests__/PageWrapper.test.tsx
+++ src/common/components/wrapper/__tests__/PageWrapper.test.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { mountWithProviders } from '../../../test/testUtils';
-import { MemoryRouter } from 'react-router';
+import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router';
 
+import store from '../../../../redux/store';
 import PageWrapper from '../PageWrapper';
 
 it('matches snapshot', () => {
-  const wrapper = mountWithProviders(
-    <MemoryRouter>
-      <PageWrapper />
-    </MemoryRouter>
+  const wrapper = mount(
+    <Provider store={store}>
+      <HelmetProvider>
+        <MemoryRouter keyLength={0}>
+          <PageWrapper />
+        </MemoryRouter>
+      </HelmetProvider>
+    </Provider>
   );
   expect(toJson(wrapper)).toMatchSnapshot();
 });
