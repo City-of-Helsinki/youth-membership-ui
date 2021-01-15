c3b61d3fb95003b86a1619a692b8608bd63db4fe
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-01-15 09:27:35 +0200

    Fix tests.

--- src/common/components/wrapper/__tests__/PageWrapper.test.tsx
+++ src/common/components/wrapper/__tests__/PageWrapper.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mountWithProviders } from '../../../test/testUtils';
+import { MemoryRouter } from 'react-router';
 import toJson from 'enzyme-to-json';
 
 import PageWrapper from '../PageWrapper';
 
 it('matches snapshot', () => {
-  const wrapper = shallow(<PageWrapper />);
+  const wrapper = mountWithProviders(
+    <MemoryRouter>
+      <PageWrapper />
+    </MemoryRouter>
+  );
   expect(toJson(wrapper)).toMatchSnapshot();
 });
