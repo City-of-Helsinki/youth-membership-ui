ef123c9d3f522cb7794458523774ef746ef57f63
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-01-22 10:22:47 +0200

    Refactor app test to use testing library

--- src/domain/app/App.test.tsx
+++ src/domain/app/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 
+import { render } from '../../common/test/testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  render(<App />);
 });
