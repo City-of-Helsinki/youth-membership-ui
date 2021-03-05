43b4e285cf2ba459206a38d6e900b8e4a84dc53c
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-03-05 11:00:10 +0200

    Refactor app history extending

--- src/domain/app/appHistory.ts
+++ src/domain/app/appHistory.ts
@@ -13,73 +13,51 @@ function getPathname<S>(
   return pathOrLocation.pathname;
 }
 
+interface HistoryFunction<S> {
+  historyFunction(path: H.Path, state?: S, language?: Language): void;
+  historyFunction(
+    location: H.LocationDescriptorObject<S>,
+    language?: Language
+  ): void;
+}
+
+function evokeHistoryFunction<S>(
+  historyFunction: HistoryFunction<S>['historyFunction'],
+  pathOrLocation: H.Path | H.LocationDescriptorObject<S>,
+  state?: S,
+  language?: Language
+) {
+  if (typeof pathOrLocation === 'string') {
+    historyFunction(pathOrLocation, state, language);
+  } else {
+    historyFunction(pathOrLocation, language);
+  }
+}
+
+function extendWithLanguageAwareness<S>(
+  historyFunction: HistoryFunction<S>['historyFunction']
+) {
+  return function(
+    pathOrLocation: H.Path | H.LocationDescriptorObject<S>,
+    state?: S,
+    language: false | Language = I18nService.language
+  ) {
+    const pathname = getPathname(pathOrLocation);
+    const isLanguageInPath = PathUtils.getIsLanguageInPath(pathname);
+
+    if (typeof language === 'string' && !isLanguageInPath) {
+      evokeHistoryFunction<S>(historyFunction, pathOrLocation, state, language);
+    } else {
+      evokeHistoryFunction<S>(historyFunction, pathOrLocation, state);
+    }
+  };
+}
+
 function extendBrowserHistoryWithLanguageAwareness(browserHistory: H.History) {
-  const push = <S>(
-    pathOrLocation: H.Path | H.LocationDescriptorObject<S>,
-    state?: S,
-    language: false | Language = I18nService.language
-  ) => {
-    const pathname = getPathname(pathOrLocation);
-    const isLanguageInPath = PathUtils.getIsLanguageInPath(pathname);
-
-    if (
-      (typeof language === 'boolean' && language === false) ||
-      isLanguageInPath
-    ) {
-      if (typeof pathOrLocation === 'string') {
-        return browserHistory.push(pathOrLocation, state);
-      }
-
-      return browserHistory.push(pathOrLocation);
-    }
-
-    if (typeof pathOrLocation === 'string') {
-      return browserHistory.push(
-        PathUtils.getPathnameWithLanguage(pathOrLocation, language),
-        state
-      );
-    }
-
-    browserHistory.push(
-      PathUtils.getLocationWithLanguage(pathOrLocation, language)
-    );
-  };
-
-  const replace = <S>(
-    pathOrLocation: H.Path | H.LocationDescriptorObject<S>,
-    state?: S,
-    language: false | Language = I18nService.language
-  ) => {
-    const pathname = getPathname(pathOrLocation);
-    const isLanguageInPath = PathUtils.getIsLanguageInPath(pathname);
-
-    if (
-      (typeof language === 'boolean' && language === false) ||
-      isLanguageInPath
-    ) {
-      if (typeof pathOrLocation === 'string') {
-        return browserHistory.replace(pathOrLocation, state);
-      }
-
-      return browserHistory.replace(pathOrLocation);
-    }
-
-    if (typeof pathOrLocation === 'string') {
-      return browserHistory.replace(
-        PathUtils.getPathnameWithLanguage(pathOrLocation, I18nService.language),
-        state
-      );
-    }
-
-    browserHistory.replace(
-      PathUtils.getLocationWithLanguage(pathOrLocation, I18nService.language)
-    );
-  };
-
   return {
     ...browserHistory,
-    push,
-    replace,
+    push: extendWithLanguageAwareness(browserHistory.push),
+    replace: extendWithLanguageAwareness(browserHistory.replace),
   };
 }
 
