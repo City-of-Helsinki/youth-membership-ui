4180754b92b7664a7ca79815220cca7bb1cc7019
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-03-15 12:27:04 +0200

    Enforce stricter typing

--- src/domain/youthProfile/form/FormikTextInput.tsx
+++ src/domain/youthProfile/form/FormikTextInput.tsx
@@ -4,10 +4,12 @@ import { TextInputProps } from 'hds-react';
 import TextInput from '../../../common/components/textInput/TextInput';
 import useField from './useField';
 
-type Props = TextInputProps;
+type Props = TextInputProps & {
+  name: string;
+};
 
 function FormikTextInput(props: Props) {
-  const [field] = useField(props.name as string);
+  const [field] = useField(props.name);
 
   return <TextInput {...field} {...props} />;
 }
